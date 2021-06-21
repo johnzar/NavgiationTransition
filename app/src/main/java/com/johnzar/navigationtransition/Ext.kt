f64c633cd74a34e8697c0a45b998839e4fff9901
john_zar <john.zar@kakaopaycorp.com>
2021-06-21 17:21:19 +0900

    최종 #3

--- app/src/main/java/com/johnzar/navigationtransition/Ext.kt
+++ app/src/main/java/com/johnzar/navigationtransition/Ext.kt
@@ -17,25 +17,3 @@ fun Context.pxTodp(pxValue: Float): Int {
     val scale = resources.displayMetrics.density
     return (pxValue / scale + 0.5f).toInt()
 }
-
-data class CenterViewItem(
-    val position:Int,
-    val differ:Int
-)
-
-/**
- * 중간에서 가장 가까운 거리를 계산하는 ItemView.
- *
- * @param itemHeights
- * @return
- */
-fun ArrayList<CenterViewItem>.getMinDifferItem(): CenterViewItem {
-    var minItem = this[0] // 기본 첫 번째는 최소 나쁨 값
-    for (i in this.indices) {
-        // 최소 나쁨 값 가져오기
-        if (this[i].differ <= minItem.differ) {
-            minItem = this[i]
-        }
-    }
-    return minItem
-}
